215ba776a53320734167c337d1c5a7ce114e768c
Paul Guaman <guaman.bryan.paul@gmail.com>
2020-12-09 11:14:57 -0500

    servicio estudiante

---
+++ src/main/java/com/clearminds/pg/bdd/test/TestServicio.java
@@ -0,0 +1,22 @@
+package com.clearminds.pg.bdd.test;
+
+import com.clearminds.pg.dtos.Estudiante;
+import com.clearminds.pg.excepciones.BDDExcepcion;
+import com.clearminds.pg.servicios.ServicioEstudiante;
+
+public class TestServicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServicioEstudiante srvEstudiante= new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(new Estudiante("Paul", "Guaman"))
+			;
+		} catch (BDDExcepcion e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			e.getMessage();
+		}
+	}
+
+}
--- src/main/java/com/clearminds/pg/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/pg/servicios/ServicioEstudiante.java
@@ -1,5 +1,13 @@
 package com.clearminds.pg.servicios;
 
+import com.clearminds.pg.dtos.Estudiante;
+import com.clearminds.pg.excepciones.BDDExcepcion;
+
 public class ServicioEstudiante  extends ServicioBase{
-	
+	public void insertarEstudiante(Estudiante estudiante) throws BDDExcepcion {
+		ServicioBase ser= new ServicioBase();
+		ser.abrirConexion();
+		System.out.println("Insertando estudiante:"+ estudiante.toString());
+		ser.cerrarConexion();
+	}
 }
